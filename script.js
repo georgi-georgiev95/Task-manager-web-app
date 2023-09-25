134fd4e8790b1fb647cc7a99cff500e51831ac2c
Georgi957 <gesh007@abv.bg>
2023-09-25 11:56:33 +0300

    Updates
    Added validation for time and implemented countdown timer.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+        tasks = JSON.parse(storedTasks);
+        displayTasks();
+    }
+});
+
 const taskInput = document.getElementById("taskInput");
 const taskTimeInput = document.getElementById("taskTimeInput");
 const taskList = document.getElementById("taskList");
@@ -6,13 +14,8 @@ let tasks = [];
 const addTaskButton = document.getElementById("addTaskButton");
 addTaskButton.addEventListener("click", addTask);
 
-window.addEventListener("DOMContentLoaded", () => {
-    const storedTasks = localStorage.getItem("tasks");
-    if (storedTasks) {
-        tasks = JSON.parse(storedTasks);
-        displayTasks();
-    }
-});
+let counter = 0;
+
 
 function addTask() {
     const task = taskInput.value;
@@ -26,6 +29,29 @@ function addTask() {
     }
 }
 
+const completedDiv = document.createElement('div');
+document.querySelector('body').appendChild(completedDiv);
+completedDiv.id = 'completed-area';
+
+const completedTasks = document.createElement('p');
+completedTasks.id = 'completed-tasks'
+completedTasks.textContent = `Completed tasks: ${counter}`;
+
+const resetButton = document.createElement('button');
+resetButton.id = 'reset-btn';
+resetButton.textContent = 'Reset couner';
+
+completedDiv.appendChild(completedTasks);
+completedDiv.appendChild(resetButton);
+
+
+resetButton.addEventListener('click', resetCounter);
+
+function resetCounter() {
+    counter = 0;
+    completedTasks.textContent = `Completed tasks: ${counter}`;
+}
+
 function displayTasks() {
     taskList.innerHTML = "";
     tasks.forEach((taskObj, index) => {
@@ -35,10 +61,39 @@ function displayTasks() {
         if (done) {
             li.classList.add("done");
         }
-
+        
+        
         const taskText = document.createElement("span");
         taskText.textContent = `${task} (scheduled for ${taskTime})`;
         li.appendChild(taskText);
+
+        //adding timer
+        const timerElement = document.createElement("p");
+        let [hours, minutes] = taskTime.split(':').map(Number);
+        
+
+        let x = setInterval(function () {
+            let date = new Date();
+            let day = date.getDate();
+            let month = date.getMonth();
+            let currentYear = date.getFullYear();
+            let countDownTo = new Date(`${month + 1} ${day}, ${currentYear} ${hours}:${minutes}:00`);
+            
+            let now = date.getTime();
+            let distance = countDownTo - now;
+            let timerHours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let timerMinutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            let timerSeconds = Math.floor((distance % (1000 * 60)) / 1000);
+            timerElement.innerHTML = timerHours + "h " + timerMinutes + "m " + timerSeconds + "s ";
+
+            if (distance < 0) {
+                clearInterval(x);
+                timerElement.innerHTML = "It's time! Hurry up!";
+            }
+        }, 1000);
+         
+        li.appendChild(timerElement)
+        //endOfTimer
 
         const markButton = document.createElement("button");
         markButton.textContent = done ? "Undo" : "Done";
@@ -46,26 +101,30 @@ function displayTasks() {
         markButton.addEventListener("click", () => toggleTaskStatus(index));
         li.appendChild(markButton);
 
+        function toggleTaskStatus(index) {
+            tasks[index].done = !tasks[index].done;
+            displayTasks();
+            if (tasks[index].done) {
+                counter++;
+            }
+            completedTasks.textContent = `Completed tasks: ${counter}`;
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+        }
+
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Delete";
         deleteButton.className = "delete-button";
         deleteButton.addEventListener("click", () => deleteTask(index));
         li.appendChild(deleteButton);
-
         taskList.appendChild(li);
+
+        function deleteTask(index) {
+            tasks.splice(index, 1);
+            displayTasks();
+
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+        }
     });
 }
 
-function toggleTaskStatus(index) {
-    tasks[index].done = !tasks[index].done;
-    displayTasks();
 
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-function deleteTask(index) {
-    tasks.splice(index, 1);
-    displayTasks();
-
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
